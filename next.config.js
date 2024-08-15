3d662ee0faad53afd17f7d503adb88770de46633
T3thr <t.theerapat33@gmail.com>
2024-08-16 03:02:14 +0700

    modify nextjs workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    // ... other configurations
-    
-    // Disable static generation for API routes
-    experimental: {
-        output: 'standalone',
-    },
+
 }
 module.exports = nextConfig
