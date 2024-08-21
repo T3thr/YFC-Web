d42ce1a21013d1bd6f18c7987233d908a0065d40
T3thr <t.theerapat33@gmail.com>
2024-08-21 19:37:09 +0700

    modify nextjs workflow

--- components/AddProductForm.jsx
+++ components/AddProductForm.jsx
@@ -10,17 +10,8 @@ import Title from '@/components/Title';
 import PhotoList from './PhotoList'
 import cloudinary from 'cloudinary'
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
 
 export default async function AddProductForm() {
-  
-  const [productSKU, setProductSKU] = useState('');
-  const [productName, setProductName] = useState('');
-  const [price, setPrice] = useState('');
 
   const formRef = useRef();
   const [files, setFiles] = useState([]);
