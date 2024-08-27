a6f9954180de1150832bf707dbe4e775da6c7942
T3thr <t.theerapat33@gmail.com>
2024-08-28 02:39:06 +0700

    modify nextjs workflow

--- app/layout.js
+++ app/layout.js
@@ -18,10 +18,10 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <title>YFC</title>
       <body className={inter.className}>
-        <div className='pt-16'>
+        <div className='lg:pt-16 md:pt-20 sm:pt-32'>
           <Header />
         </div>
-        <div className='pt-16'>
+        <div className='lg:pt-16 md:pt-20 sm:pt-32'>
           {children}
         </div>
       </body>
