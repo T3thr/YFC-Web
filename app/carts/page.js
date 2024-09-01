3126e0ebf3a7e04d9d935e2771e2ad41fa34f085
T3thr <t.theerapat33@gmail.com>
2024-09-01 19:54:49 +0700

    modify nextjs workflow

--- app/carts/page.js
+++ app/carts/page.js
@@ -18,7 +18,7 @@ export default function CartPage() {
       <div className='flex flex-col justify-start items-center mx-auto h-screen my-5'>
       <div className='text-3xl'>Please Sign In!!!</div>
       <Link 
-      href={'api/auth/signin?callbackUrl=/profile'}
+      href={'/signin?callbackUrl=/carts'}
       className='ring-blue-400 ring-1 text-blue-500 rounded-sm shadow-sm py-1 px-2 my-4'
       >
           Sign In
