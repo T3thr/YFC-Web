be3c70096cbe478fe06cbc8b69bfdb4ab616a526
T3thr <t.theerapat33@gmail.com>
2024-10-07 22:38:33 +0700

    modify nextjs workflow

--- app/carts/layout.js
+++ app/carts/layout.js
@@ -1,10 +1,13 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
+import { GlobalProvider } from "@/app/GlobalProvider"
 
 export default function Layout({children}) {
     return(
+        <GlobalProvider>
         <SessionProvider>
             {children}
         </SessionProvider>
+        </GlobalProvider>
     )
 }
